cbebe510f41c1c54bd01e64d2a52de53fa781588
kai <lovefamilydev@gmail.com>
2013-12-27 00:27:36 +0900

    change root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Mrsmart::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.relative_url_root = "/mrsmart"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Mrsmart::Application.routes.draw do
-  resources :faqs
+  scope '/mrsmart' do
+    resources :faqs
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
