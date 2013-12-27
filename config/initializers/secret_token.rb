263ec0edb09e9969b7852b94ca044b9ec97bf557
kai <lovefamilydev@gmail.com>
2013-12-27 18:33:07 +0900

    no message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mrsmart::Application.config.secret_token = 'c267548921a8e4b342b45e5743ee7027b6c6df0045dae3fb1bc2fb1ee3bf24f3f052c725992e171bc386f8c1802563a8f469ea71560a64fb6b06f2b33194ec53'
